b76d9fdd8688eaea51ff5da7b9f1b33073299da7
Javier García <javigcpgr@gmail.com>
2023-10-19 10:11:25 +0200

    fix(changes vercel)

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -12,12 +12,12 @@ type AppConfig = {
     PORT: string | number
 }
 
-if (process.env.NODE_ENV === 'production') {
+/*if (process.env.NODE_ENV === 'production') {
     dotenv.config({ path: '.env.production' })
 } else {
     dotenv.config({ path: '.env.development' })
 }
-
+*/
 const ENV = process.env.NODE_ENV ?? "development"
 
 const CONFIG: any = {
