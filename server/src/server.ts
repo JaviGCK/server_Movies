3be18cafa33a350351b6cdff205d28d4315970d9
Javier García <javigcpgr@gmail.com>
2023-10-19 09:29:53 +0200

    fix(vercel)

--- server/src/server.ts
+++ server/src/server.ts
@@ -22,9 +22,9 @@ app.use('/users', usersRoutes);
 app.use('/movies', moviesRoutes);
 app.use('/genres', genresRoutes);
 
-/*app.get("/", (req: Request, res: Response) => {
+app.get("/", (req: Request, res: Response) => {
     res.status(200).json({ message: "Welcome to the API World" });
-});*/
+});
 
 
 
